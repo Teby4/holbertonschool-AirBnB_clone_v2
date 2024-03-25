e0d525a951a9b513129d60084933c00dff564fc1
root <felipeoliveramantero@gmail.com>
2024-03-25 07:38:08 -0700

    task

--- console.py
+++ console.py
@@ -144,23 +144,35 @@ class HBNBCommand(cmd.Cmd):
         print("Creates a class of any type")
         print("[Usage]: create <className>\n")
 
-    def do_show(self, arg):
-        if arg == "":
+    def do_show(self, args):
+        """ Method to show an individual object """
+        new = args.partition(" ")
+        c_name = new[0]
+        c_id = new[2]
+
+        # guard against trailing args
+        if c_id and ' ' in c_id:
+            c_id = c_id.partition(' ')[0]
+
+        if not c_name:
             print("** class name missing **")
-        else:
-            args = arg.split()
-            cla = args[0]
-            if cla not in classes:
-                print("** class doesn't exist **")
-            elif len(args) != 2:
-                print("** instance id missing **")
-            else:
-                instanceList = models.storage.all()
-                key = "{}.{}".format(args[0], args[1])
-                if key in instanceList:
-                    print(str(instanceList[key]))
-                else:
-                    print("** no instance found **")
+            return
+
+        if c_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        if not c_id:
+            print("** instance id missing **")
+            return
+
+        instanceList = models.storage.all()
+        key = c_name + "." + c_id
+        try:
+            if key in instanceList:
+                print(str(instanceList[key]))
+        except KeyError:
+            print("** no instance found **")
 
     def help_show(self):
         """ Help information for the show command """
