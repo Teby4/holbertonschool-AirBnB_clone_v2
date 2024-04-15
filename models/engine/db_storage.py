fd135671d0a54cc03dbf0e88402be0e024139799
root <felipeoliveramantero@gmail.com>
2024-04-14 18:12:50 -0700

    tasks

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -48,3 +48,8 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         self.__session = scoped_session(session_factory)
+    
+    
+    def close(self):
+        self.reload()
+        self.__session.close()
